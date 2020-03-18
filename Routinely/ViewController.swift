5598df16f7567e08cf178b2e9429b382539fc892
Aaron Rosenfeld <aaronrosenfeld@stockx.com>
2020-03-18 19:04:33 -0400

    Initial Commit

---
+++ Routinely/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Routinely
+//
+//  Created by Aaron Rosenfield on 3/18/20.
+//  Copyright © 2020 Aaron Rosenfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
