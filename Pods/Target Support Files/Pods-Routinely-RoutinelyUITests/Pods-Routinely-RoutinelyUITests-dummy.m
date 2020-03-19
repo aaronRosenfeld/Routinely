9559d3d57be9e03af7ad069bfe4e75070fa981da
Aaron Rosenfeld <aaronrosenfeld@stockx.com>
2020-03-18 20:36:00 -0400

    add snapkit

---
+++ Pods/Target Support Files/Pods-Routinely-RoutinelyUITests/Pods-Routinely-RoutinelyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Routinely_RoutinelyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Routinely_RoutinelyUITests
+@end
---
+++ Pods/Target Support Files/Pods-RoutinelyTests/Pods-RoutinelyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RoutinelyTests : NSObject
+@end
+@implementation PodsDummy_Pods_RoutinelyTests
+@end
